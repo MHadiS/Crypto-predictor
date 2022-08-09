67995043ee34b168b8a8c61341d7d93d144fb82e
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-08-09 14:25:09 +0430

    DOCS: add docstring to get_data method

--- utils/data_collector/data_collector.py
+++ utils/data_collector/data_collector.py
@@ -11,5 +11,12 @@ class DataCollector:
     A class for collecting currencies data to train the models
     """
     def get_data(self, start_date: datetime, end_date: datetime, ticker: str):
+        """Get the finances data with yfinance api
+
+        Args:
+            start_date (datetime): start date of data
+            end_date (datetime): end data of data
+            ticker (str): the symbol for the finance
+        """
         data: pd.DataFrame = yf.download(ticker, start=start_date, end=end_date)
         data.to_csv(f"../data/{ticker}.csv")
