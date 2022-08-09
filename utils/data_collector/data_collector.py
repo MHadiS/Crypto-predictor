ed52401f18239f6667d0cc986c0a63b79d8eef2a
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-08-09 14:18:25 +0430

    FEAT: add get_data method to DataCollector class

--- utils/data_collector/data_collector.py
+++ utils/data_collector/data_collector.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import yfinance as yf
 from datetime import datetime
 import pandas as pd
@@ -9,3 +10,6 @@ class DataCollector:
     """
     A class for collecting currencies data to train the models
     """
+    def get_data(self, start_date: datetime, end_date: datetime, ticker: str):
+        data: pd.DataFrame = yf.download(ticker, start=start_date, end=end_date)
+        data.to_csv(f"../data/{ticker}.csv")
