6083a9176922afab0400cfc7e9610863d910ede7
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-08-10 18:24:25 +0430

    TEST: write test for generate_model method

--- tests/test_model_generator.py
+++ tests/test_model_generator.py
@@ -11,3 +11,9 @@ def model_generator():
 def test_read_data(model_generator): 
     global X_train, X_test, X_val, y_train, y_test, y_val
     X_train, X_test, X_val, y_train, y_test, y_val = model_generator.read_data("BTC-USD")
+
+
+def test_generate_model(model_generator):
+    model = model_generator.generate_model("BTC-USD")
+    score = model.score(X_test, y_test)
+    assert int(score * 100) > 90
