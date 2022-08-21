4d950fa80ded9291f7b7a26a621859a510af6ee4
Mohammad Hadi Shakibazad <hadishakib6561@gmail.com>
2022-08-21 19:02:37 +0430

    FEAT: change the learning algorithm and add predict method

--- utils/model_generator/model_generator.py
+++ utils/model_generator/model_generator.py
@@ -3,7 +3,8 @@ import numpy as np
 import pickle as pkl
 import utils.settings as s
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
 
 class ModelGenerator:
@@ -20,9 +21,12 @@ class ModelGenerator:
         """
         df = pd.read_csv(f"utils/data/{ticker}.csv")
         X = np.array([i for i in range(df.shape[0])]).reshape(-1, 1)
+        self.degree = 10
+        self.poly_reg = PolynomialFeatures(degree=self.degree)
+        X_poly = self.poly_reg.fit_transform(X)
         y = df["Close"].values.reshape(-1, 1)
         X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, shuffle=False
+            X_poly, y, test_size=0.2, shuffle=False
         )
         X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.1)
         return X_train, X_test, X_val, y_train, y_test, y_val
@@ -37,7 +41,7 @@ class ModelGenerator:
             Unknown for now: the model
         """
         X_train, X_test, X_val, y_train, y_test, y_val = self.read_data(currency)
-        model = DecisionTreeRegressor()
+        model = LinearRegression()
         model.fit(X_train, y_train)
         return model
 
@@ -50,6 +54,19 @@ class ModelGenerator:
         """
         with open(f"utils/models/{currency}.model", "wb") as f:
             pkl.dump(model, f)
+    
+    def predict(self, model, x: np.array) -> np.array:
+        """Make prediction with the model model
+
+        Args:
+            model (Unknown for now): the model
+            x (np.array): the input of the model
+
+        Returns:
+            np.array: an array of the model predictions
+        """
+        x_poly = self.poly_reg.fit_transform(x)
+        return model.predict(x_poly)
 
     def update(self):
         """Update the models with the new data"""
